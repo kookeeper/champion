cf354f05405d012c13b7c9b89e112277f5970ef0
Sérgio Paulo Samaan Filho <sergio.filho@multivarejogpa.com.br>
2016-06-15 13:53:20 -0300

    import inicial

---
+++ cordova/champion/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.rjfun.cordova.httpd/www/CorHttpd.js",
+        "id": "com.rjfun.cordova.httpd.CorHttpd",
+        "clobbers": [
+            "cordova.plugins.CorHttpd"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "com.rjfun.cordova.httpd": "0.9.2"
+};
+// BOTTOM OF METADATA
+});
